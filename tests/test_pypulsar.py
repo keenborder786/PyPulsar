6ebf4245bcffbe74f07770050f0a28de9a4c1345
keenborder786 <21110290@lums.edu.pk>
2023-02-04 01:13:41 +0500

    [test] Added tests for cluster

--- tests/test_pypulsar.py
+++ tests/test_pypulsar.py
@@ -1,1 +1,8 @@
-from pypulsar import pypulsar
+from pypulsar import Cluster
+from pandas import DataFrame
+
+def test_cluster():
+    pulsar_cluster = Cluster(False , 'localhost' , '8080')
+    bookie_info = pulsar_cluster.all_bookie_info()
+    print(pulsar_cluster.get_rack_placement_bookies())
+    assert isinstance(bookie_info , DataFrame) == True
