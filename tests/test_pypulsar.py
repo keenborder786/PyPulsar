bafa1db9527c98ec625895c08137d5af3852a24e
keenborder786 <21110290@lums.edu.pk>
2023-02-06 11:44:18 +0500

    [test] corrected the hostname in test_pypulsar

--- tests/test_pypulsar.py
+++ tests/test_pypulsar.py
@@ -4,14 +4,14 @@ from pypulsar import Bookie, Cluster
 
 
 def test_cluster():
-    pulsar_cluster = Cluster(False, "pulsar", "8080")
+    pulsar_cluster = Cluster(False, "localhost", "8080")
     assert isinstance(pulsar_cluster.all_bookie_info(), DataFrame) == True
     assert isinstance(
         pulsar_cluster.get_rack_placement_bookies(), dict) == True
 
 
 def test_bookies():
-    pulsar_bookie = Bookie(False, "pulsar", "8080", "127.0.0.1:3181")
+    pulsar_bookie = Bookie(False, "localhost", "8080", "127.0.0.1:3181")
     assert isinstance(
         pulsar_bookie.delete_rack_placement_bookie(), dict) == True
     assert isinstance(pulsar_bookie.get_rack_placement_bookie(), dict) == True
