7282fe18b48911b37d98f12548afd84a30370796
keenborder786 <21110290@lums.edu.pk>
2023-02-15 00:19:27 +0500

    [update]: Added Broker in PyPulsar

--- src/pypulsar/__init__.py
+++ src/pypulsar/__init__.py
@@ -2,6 +2,7 @@
 from importlib.metadata import version
 
 from pypulsar.bookie import Bookie
+from pypulsar.broker import Broker
 from pypulsar.cluster import Cluster
 
 __version__ = version("pypulsar")
