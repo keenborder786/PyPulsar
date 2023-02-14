b4696bde1ed14aef394e6e281d8d869d3b2832f7
keenborder786 <21110290@lums.edu.pk>
2023-02-15 00:20:57 +0500

    [Tests]: Added Tests for Broker Class

--- tests/test_pypulsar.py
+++ tests/test_pypulsar.py
@@ -1,6 +1,6 @@
 from pandas import DataFrame
 
-from pypulsar import Bookie, Cluster
+from pypulsar import Bookie, Broker, Cluster
 
 
 def test_cluster():
@@ -8,8 +8,6 @@ def test_cluster():
     assert isinstance(pulsar_cluster.all_bookie_info(), DataFrame) == True
     assert isinstance(
         pulsar_cluster.get_rack_placement_bookies(), dict) == True
-    assert isinstance(pulsar_cluster.get_broker_stats_allocator(
-        "default"), dict) == True
     assert (
         isinstance(
             pulsar_cluster.get_pending_bookie_client_op_stats(), DataFrame) == True
@@ -26,10 +24,6 @@ def test_cluster():
             "cursorLedgerDeleteOp",
         ]
     ) or isinstance(pulsar_cluster.get_pending_bookie_client_op_stats(), dict) == True
-    assert (
-        isinstance(pulsar_cluster.get_broker_availability_report(
-            "public", "default"), dict) == True
-    )
 
 
 def test_bookies():
@@ -46,3 +40,47 @@ def test_bookies():
         )
         == True
     )
+
+
+def test_brokers():
+    pulsar_broker = Broker(False, "localhost", "8080")
+    assert (
+        isinstance(pulsar_broker.get_broker_availability_report(
+            "public", "default"), dict) == True
+    )
+    assert isinstance(pulsar_broker.get_broker_stats_allocator(
+        "default"), dict) == True
+    broker_load = pulsar_broker.get_broker_load()
+    if broker_load != {}:
+        assert isinstance(broker_load, dict) == True and set(list(broker_load.keys())) == set(
+            [
+                "webServiceUrl",
+                "pulsarServiceUrl",
+                "persistentTopicsEnabled",
+                "nonPersistentTopicsEnabled",
+                "cpu",
+                "memory",
+                "directMemory",
+                "bandwidthIn",
+                "bandwidthOut",
+                "msgThroughputIn",
+                "msgThroughputOut",
+                "msgRateIn",
+                "msgRateOut",
+                "lastUpdate",
+                "lastStats",
+                "numTopics",
+                "numBundles",
+                "numConsumers",
+                "numProducers",
+                "bundles",
+                "lastBundleGains",
+                "lastBundleLosses",
+                "brokerVersionString",
+                "protocols",
+                "advertisedListeners",
+                "bundleStats",
+                "loadReportType",
+                "maxResourceUsage",
+            ]
+        )
