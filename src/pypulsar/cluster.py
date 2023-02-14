5fa98e68640f4ff1d35c4d9f55783b3936080c5e
keenborder786 <21110290@lums.edu.pk>
2023-02-15 00:20:38 +0500

    [Update]: Moved Broker Specific Method to Newly Created Broker Class

--- src/pypulsar/cluster.py
+++ src/pypulsar/cluster.py
@@ -81,36 +81,6 @@ class Cluster:
                 f"Request could not happen due to the following to error {response.status_code}")
         return {}
 
-    def get_broker_stats_allocator(self, allocator: str) -> Dict:
-        """
-
-        Parameters:
-        --------------------------------------
-
-        allocator(str): Available allocators are 'default' and 'ml-cache'
-
-
-        Get the stats for the Netty allocator. Available allocators are 'default' and 'ml-cache'
-
-        Returns:
-        --------------------------------------
-        Dict: Stats for the Netty allocator in form of dictionary
-
-        """
-        response = requests.get(
-            f"{self.request_type}://{self.webservice_url}:{self.webservice_port}/admin/v2/broker-stats/allocator-stats/{allocator}"
-        )
-        if response.status_code == 200:
-            final_response = json.loads(response.content)
-            return final_response
-        elif response.status_code == 403:
-            print("Don't have admin permission")
-        else:
-            print(
-                f"Request could not happen due to the following to error {response.status_code}")
-
-        return {}
-
     def get_pending_bookie_client_op_stats(self) -> Union[DataFrame, Dict]:
         """
 
@@ -140,39 +110,6 @@ class Cluster:
                     final_dataframe_data.append(instance)
             df_final_response = pd.DataFrame(final_dataframe_data)
             return df_final_response
-        elif response.status_code == 403:
-            print("Don't have admin permission")
-        else:
-            print(
-                f"Request could not happen due to the following to error {response.status_code}")
-
-        return {}
-
-    def get_broker_availability_report(self, tenant: str, namespace: str) -> Dict:
-        """
-
-        This API gives the current broker availability in percent, each resource percentage usage is calculated and
-        thensum of all of the resource usage percent is called broker-resource-availability
-
-        Parameters:
-        --------------------
-        tenant(str): The tenant for which you want availability report
-
-        namespace(str): The namespace for which want availability report
-
-        Returns:
-        --------------------
-        dict: The broker availability information in form of dictionary
-
-        """
-
-        response = requests.get(
-            f"{self.request_type}://{self.webservice_url}:{self.webservice_port}/admin/v2/broker-stats/broker-resource-availability/{tenant}/{namespace}"
-        )
-
-        if response.status_code == 200:
-            final_response = json.loads(response.content)
-            return final_response
         elif response.status_code == 403:
             print("Don't have admin permission")
         else:
